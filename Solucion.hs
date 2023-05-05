14911f5e99d7d18023b732f9bb0bd94c7c5e3051
benja.fcen <132655142+benja-fcen@users.noreply.github.com>
2023-05-05 17:09:39 -0300

    Solución ejercicio 1
    1. Nombres de archivos cambiados, Haskell no puede importar archivos que contiene "-" en su nombre.
    
    2. Agregado archivo "Solucion" que contiene contiene las funciones que deben ser testeadas en "testcatedra.hs"
    
    3. Para testear se debe usar "runTestTT tests" va a devolver algo así como "10 intentos 9 errores 0 fallos".
    
    4. Para testear manualmente las funciones del archivo soluciones simplemente hacer un llamado desde prelude, por ejemplo "nombresDeUsuarios redA"

---
+++ Solucion.hs
@@ -0,0 +1,46 @@
+module Solucion where
+import TP
+    
+nombresDeUsuarios :: RedSocial -> [String]
+nombresDeUsuarios rs = [nombresDeUsuarios' (usuarios rs)]
+
+nombresDeUsuarios' :: [Usuario] -> String
+nombresDeUsuarios' [] = ""
+nombresDeUsuarios' [u] = nombreDeUsuario u
+nombresDeUsuarios' (u:us) = nombreDeUsuario u ++ " " ++ nombresDeUsuarios' us
+
+-- describir qué hace la función: .....
+amigosDe :: RedSocial -> Usuario -> [Usuario]
+amigosDe = undefined
+
+-- describir qué hace la función: .....
+cantidadDeAmigos :: RedSocial -> Usuario -> Int
+cantidadDeAmigos = undefined
+
+-- describir qué hace la función: .....
+usuarioConMasAmigos :: RedSocial -> Usuario
+usuarioConMasAmigos = undefined
+
+-- describir qué hace la función: .....
+estaRobertoCarlos :: RedSocial -> Bool
+estaRobertoCarlos = undefined
+
+-- describir qué hace la función: .....
+publicacionesDe :: RedSocial -> Usuario -> [Publicacion]
+publicacionesDe = undefined
+
+-- describir qué hace la función: .....
+publicacionesQueLeGustanA :: RedSocial -> Usuario -> [Publicacion]
+publicacionesQueLeGustanA = undefined
+
+-- describir qué hace la función: .....
+lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
+lesGustanLasMismasPublicaciones = undefined
+
+-- describir qué hace la función: .....
+tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
+tieneUnSeguidorFiel = undefined
+
+-- describir qué hace la función: .....
+existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
+existeSecuenciaDeAmigos = undefined
