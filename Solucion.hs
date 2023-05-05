a7dfb35840338c41a020a2bed8094a01765cef56
benja.fcen <132655142+benja-fcen@users.noreply.github.com>
2023-05-05 18:59:56 -0300

    Resuelto ejercicio 2
    Agregada la solucion del ejercicio 2 y aplicadas correciones de formato. Añadidos comentarios describiendo brevemente que hace cada solucion.

--- Solucion.hs
+++ Solucion.hs
@@ -1,17 +1,29 @@
 module Solucion where
 import TP
-    
+
+-- Devuelve los nombres de usuarios en una lista: ["nombre1", "nombre2", ...]
 nombresDeUsuarios :: RedSocial -> [String]
 nombresDeUsuarios rs = nombresDeUsuarios' (usuarios rs)
 
 nombresDeUsuarios' :: [Usuario] -> [String]
-nombresDeUsuarios' [] = []
-nombresDeUsuarios' [u] = [nombreDeUsuario u]
+nombresDeUsuarios' []     = []
+nombresDeUsuarios' [u]    = [nombreDeUsuario u]
 nombresDeUsuarios' (u:us) = [nombreDeUsuario u] ++ nombresDeUsuarios' us
 
--- describir qué hace la función: .....
+-- Duevuelve una lista de usuarios que estan relacionados con un usuario especifico.
 amigosDe :: RedSocial -> Usuario -> [Usuario]
-amigosDe = undefined
+amigosDe rs usuario = amigosDe' (relaciones rs) usuario
+
+amigosDe' :: [Relacion] -> Usuario -> [Usuario]
+amigosDe' [] _ = []
+amigosDe' ((u1,u2):[]) usuario
+    | usuario == u1 = [u2]
+    | usuario == u2 = [u1]
+    | otherwise     = []
+amigosDe' ((u1,u2):rs) usuario
+    | usuario == u1 = [u2] ++ amigosDe' rs usuario
+    | usuario == u2 = [u1] ++ amigosDe' rs usuario
+    | otherwise     = amigosDe' rs usuario
 
 -- describir qué hace la función: .....
 cantidadDeAmigos :: RedSocial -> Usuario -> Int
