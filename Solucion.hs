6bdbb3871d87f29c6616e1ee287b1f757c8b975c
benja.fcen <132655142+benja-fcen@users.noreply.github.com>
2023-05-08 18:50:04 -0300

    Corrección del archivo Solucion.hs

--- Solucion.hs
+++ Solucion.hs
@@ -1,58 +1,85 @@
 module Solucion where
-import TP
+-- Ejercicios
 
--- Devuelve los nombres de usuarios en una lista: ["nombre1", "nombre2", ...]
+-- Devuelve un [String] cuyos elementos son los nombres de cada usuario de la red
 nombresDeUsuarios :: RedSocial -> [String]
-nombresDeUsuarios rs = nombresDeUsuarios' (usuarios rs)
+nombresDeUsuarios rs = eliminarRepetidos (proyectarNombres (usuarios rs))
 
-nombresDeUsuarios' :: [Usuario] -> [String]
-nombresDeUsuarios' []     = []
-nombresDeUsuarios' [u]    = [nombreDeUsuario u]
-nombresDeUsuarios' (u:us) = [nombreDeUsuario u] ++ nombresDeUsuarios' us
-
--- Duevuelve una lista de usuarios que estan relacionados con un usuario especifico.
+-- Devuelve una lista de usuarios que estan relacionados con un usuario especifico
 amigosDe :: RedSocial -> Usuario -> [Usuario]
-amigosDe rs usuario = amigosDe' (relaciones rs) usuario
+amigosDe rsRed uUsuario = amigosDe' (relaciones rsRed) uUsuario
 
 amigosDe' :: [Relacion] -> Usuario -> [Usuario]
 amigosDe' [] _ = []
-amigosDe' ((u1,u2):[]) usuario
-    | usuario == u1 = [u2]
-    | usuario == u2 = [u1]
-    | otherwise     = []
-amigosDe' ((u1,u2):rs) usuario
-    | usuario == u1 = [u2] ++ amigosDe' rs usuario
-    | usuario == u2 = [u1] ++ amigosDe' rs usuario
-    | otherwise     = amigosDe' rs usuario
+amigosDe' [(u1,u2)] uUsuario
+    | uUsuario == u1 = [u2]
+    | uUsuario == u2 = [u1]
+    | otherwise      = []
+amigosDe' ((u1,u2):rRelaciones) uUsuario
+    | uUsuario == u1 = u2:amigosDe' rRelaciones uUsuario
+    | uUsuario == u2 = u1:amigosDe' rRelaciones uUsuario
+    | otherwise      = amigosDe' rRelaciones uUsuario
 
--- describir qué hace la función: .....
-cantidadDeAmigos :: RedSocial -> Usuario -> Int
-cantidadDeAmigos = undefined
+-- proyectarNombres: Toma una lista de Usuarios y devuelve
+-- una lista de String donde cada elemento es el nombre 
+-- de usuario de cada uno de los elementos de la lista de
+-- Usuarios.
+proyectarNombres :: [Usuario] -> [String]
+proyectarNombres []     = [] 
+proyectarNombres [u]    = [nombreDeUsuario u]
+proyectarNombres (u:us) = [nombreDeUsuario u] ++ proyectarNombres us
 
--- describir qué hace la función: .....
-usuarioConMasAmigos :: RedSocial -> Usuario
-usuarioConMasAmigos = undefined
+-- pertenece: Verifica si un elemento pertenece
+-- a una lista de elementos del mismo tipo. 
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece _ [] = False
+pertenece x [y] = x == y
+pertenece x (y:ys)
+    | x == y    = True
+    | otherwise = pertenece x ys
 
--- describir qué hace la función: .....
-estaRobertoCarlos :: RedSocial -> Bool
-estaRobertoCarlos = undefined
+-- tieneRepetidos: Verifica si una lista tiene una elemento
+-- mas de una véz
+tieneRepetidos :: (Eq t) => [t] -> Bool
+tieneRepetidos [] = False
+tieneRepetidos [x] = False
+tieneRepetidos (x:xs)
+    | pertenece x xs  = True
+    | otherwise       = tieneRepetidos xs
 
--- describir qué hace la función: .....
-publicacionesDe :: RedSocial -> Usuario -> [Publicacion]
-publicacionesDe = undefined
+-- eliminarRepetidos: Toma una lista de algun tipo
+-- y la devuelve pero cada elemento aparece una única vez.
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos []  = []
+eliminarRepetidos [x] = [x]
+eliminarRepetidos (x:xs)
+    | not (tieneRepetidos (x:xs)) = (x:xs)
+    | pertenece x xs              = eliminarRepetidos xs
+    | otherwise                   = x:eliminarRepetidos xs
 
--- describir qué hace la función: .....
-publicacionesQueLeGustanA :: RedSocial -> Usuario -> [Publicacion]
-publicacionesQueLeGustanA = undefined
+type Usuario = (Integer, String) -- (id, nombre)
+type Relacion = (Usuario, Usuario) -- usuarios que se relacionan
+type Publicacion = (Usuario, String, [Usuario]) -- (usuario que publica, texto publicacion, likes)
+type RedSocial = ([Usuario], [Relacion], [Publicacion])
 
--- describir qué hace la función: .....
-lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
-lesGustanLasMismasPublicaciones = undefined
+-- Funciones basicas
+usuarios :: RedSocial -> [Usuario]
+usuarios (us, _, _) = us
 
--- describir qué hace la función: .....
-tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
-tieneUnSeguidorFiel = undefined
+relaciones :: RedSocial -> [Relacion]
+relaciones (_, rs, _) = rs
 
--- describir qué hace la función: .....
-existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
-existeSecuenciaDeAmigos = undefined
+publicaciones :: RedSocial -> [Publicacion]
+publicaciones (_, _, ps) = ps
+
+idDeUsuario :: Usuario -> Integer
+idDeUsuario (id, _) = id 
+
+nombreDeUsuario :: Usuario -> String
+nombreDeUsuario (_, nombre) = nombre 
+
+usuarioDePublicacion :: Publicacion -> Usuario
+usuarioDePublicacion (u, _, _) = u
+
+likesDePublicacion :: Publicacion -> [Usuario]
+likesDePublicacion (_, _, us) = us
