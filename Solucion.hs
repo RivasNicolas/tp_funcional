0d37b3e2817426a586cb59c47621f0782ebd07a0
benja.fcen <132655142+benja-fcen@users.noreply.github.com>
2023-05-17 00:42:17 -0300

    (No mergear) Cambios al EJ10 y mas
    -- NO MERGEAR, esto lo subo como checkpoint para no perderme.
    -- Generalizada la funcion cantidadDeElementosLisa
    -- Cambios en el ejercicio 10, falta comentar y borrar la versión vieja.
    -- Agregada la función principio.
    -- Agregada la función quitarVarios
    -- Agregada la función cadenaDeAmigos2 y existeSecuenciaDeAmigos2, van a reemplazar a las originales.

--- Solucion.hs
+++ Solucion.hs
@@ -97,7 +97,7 @@ cantidadDeAmigos :: RedSocial -> Usuario -> Int
 cantidadDeAmigos rs usuario = cantidadDeElementosLista (amigosDe rs usuario)
 
 -- Hago una funcion auxiliar que cuente la cantidad de elementos que tengo en una lista
-cantidadDeElementosLista :: [Usuario] -> Int
+cantidadDeElementosLista :: (Eq t) => [t] -> Int
 cantidadDeElementosLista [] = 0
 cantidadDeElementosLista [x] = 1
 cantidadDeElementosLista (x:xs) = 1 + cantidadDeElementosLista xs 
@@ -132,6 +132,129 @@ lesGustanLasMismasPublicaciones = undefined
 tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
 tieneUnSeguidorFiel = undefined
 
--- describir qué hace la función: .....
+tieneUnSeguidorFiel' :: RedSocial -> Usuario -> [Usuario] -> Bool
+tieneUnSeguidorFiel' _ _ [] = False
+tieneUnSeguidorFiel' rsRed uUsuario (u:us)
+    | publicacionesDe rsRed uUsuario == []                                               = False -- Siempre devolvia True si un usuario no tenia publicaciones, pues esSubconjunto [] [a,b,...] siempre devuelve True
+    | uUsuario == u                                                                      = tieneUnSeguidorFiel' rsRed uUsuario us
+    | esSubconjunto (publicacionesDe rsRed uUsuario) (publicacionesQueLeGustanA rsRed u) = True
+    | otherwise                                                                          = tieneUnSeguidorFiel' rsRed uUsuario us
+
+-- Verifica que TODOS los elementos de una lista A pertenecen a una lista B
+esSubconjunto :: (Eq t) => [t] -> [t] -> Bool
+esSubconjunto _ [] = False
+esSubconjunto [] _ = True
+esSubconjunto (x:xs) (y:ys)
+    | pertenece x (y:ys) = esSubconjunto xs (y:ys)
+    | otherwise          = False
+
+-- Toma un redSocial y dos usuarios, y verifica si existe una cadena de amigos que relaciona
+-- indirectamente a estos usuarios, por ejemplo:
+-- U1 es amigo de U2, U2 es amigo de U3
+-- Si hacemos "existeSecuenciaDeAmigos <RedSocial> U1 U3", nos devolvera verdadero, pues hay
+-- una cadena de amigos entre U1 y U3
+-- ## MARCADA_PARA_ELIMINAR
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
-existeSecuenciaDeAmigos = undefined
+existeSecuenciaDeAmigos rsRed uU1 uU2
+    | (pertenece uU1 (usuarios rsRed) && pertenece uU2 (usuarios rsRed)) == False = False
+    | otherwise = existeSecuenciaDeAmigos' (usuarios rsRed) uU1 uU2 rsRed
+-- ## MARCADA_PARA_ELIMINAR
+existeSecuenciaDeAmigos' :: [Usuario] -> Usuario -> Usuario -> RedSocial -> Bool
+existeSecuenciaDeAmigos' us uU1 uU2 rsRed
+    | not (empiezaCon uU1 us) = existeSecuenciaDeAmigos' (moverAlPrincipio uU1 us) uU1 uU2 rsRed
+    | not (terminaCon uU2 us) = existeSecuenciaDeAmigos' (moverAlFinal uU2 us) uU1 uU2 rsRed
+    | otherwise               = cadenaDeAmigos us rsRed
+
+-- ## MARCADA_PARA_ELIMINAR
+-- Verifica si el primer elemento de una lista [t] es t
+empiezaCon :: (Eq t) => t -> [t] -> Bool
+empiezaCon x (y:_) = x == y
+
+lista1 = []
+-- Toma un elemento t y una lista [t] y :
+-- Si el elemento t existe en la lista, toma el elemento mas a la izquierda de la lista y lo mueve al principio
+-- Si el elemento t no existe en la lista, agrega t al principio de la lista.
+moverAlPrincipio :: (Eq t) => t -> [t] -> [t]
+moverAlPrincipio x []     = [x]
+moverAlPrincipio x (y:ys) = x:quitar x (y:ys)
+
+-- ## MARCADA_PARA_ELIMINAR
+-- Verifica si el último elemento de una lista [t] es t
+terminaCon :: (Eq t) => t -> [t] -> Bool
+terminaCon x (y:ys) = x == ultimoElem (y:ys)
+
+-- Devuelve el último elemento de una lista
+ultimoElem :: (Eq t) => [t] -> t
+ultimoElem [x] = x
+ultimoElem (x:xs) = ultimoElem xs
+
+-- Toma un elemento t y una lista [t] y :
+-- Si el elemento t existe en la lista, toma el elemento mas a la izquierda de la lista y lo mueve al fondo
+-- Si el elemento t no existe en la lista, agrega t al final de la lista.
+moverAlFinal :: (Eq t) => t -> [t] -> [t]
+moverAlFinal x []     = [x]
+moverAlFinal x (y:ys) = quitar x (y:ys) ++ [x]
+
+-- Borra el primer t (empezando desde la izquierda).
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar _ [] = []
+quitar x (y:ys)
+    | x == y = ys
+    | otherwise = y:quitar x ys
+
+-- ## MARCADA_PARA_ELIMINAR
+-- Borra varios elementos de un conjunto t (empezando desde la izquierda)
+quitarVarios :: (Eq t) => [t] -> [t] -> [t]
+quitarVarios _ [] = []
+quitarVarios [] t = t
+quitarVarios (x:xs) ys = quitarVarios xs (quitar x ys)
+-- Toma una lista de usuarios liU y un RedSocial y verifica si existe una cadena de amigos entre
+-- todos los Usuarios en liU.
+-- Por ejemplo [U1, U2, U3, U4], si U1 esta relacionado con U2, U2 con U3 y U3 con U4, entonces
+-- devuelve verdadero.
+cadenaDeAmigos :: [Usuario] -> RedSocial -> Bool
+cadenaDeAmigos [u1, u2] rsRed = relacionadosDirecto u1 u2 rsRed
+cadenaDeAmigos (u1:u2:us) rsRed
+    | cantidadDeAmigos rsRed u1 == 0  = False
+    | not (perteneceAlgunElemDe (amigosDe rsRed u2) us) && relacionadosDirecto u1 u2 rsRed = cadenaDeAmigos (u1:us) rsRed
+    | relacionadosDirecto u1 u2 rsRed                                                      = cadenaDeAmigos (u2:us) rsRed
+    | otherwise                                                                            = cadenaDeAmigos ((u1:us) ++ [u2]) rsRed
+
+
+ 
+-- Toma dos usuarios U1, U2 y toma una RedSocial y verifica si existe, una dupla Relación
+-- en RedSocial que contiene U1 y U2 como sus elementos.
+relacionadosDirecto :: Usuario -> Usuario -> RedSocial -> Bool
+relacionadosDirecto uU1 uU2 rsRed = pertenece (uU1, uU2) (relaciones rsRed) || pertenece (uU2, uU1) (relaciones rsRed)
+
+-- Verificia si por lo menos un elemento de una lista [t], pertenece a otra lista [t]
+perteneceAlgunElemDe :: (Eq t) => [t] -> [t] -> Bool
+perteneceAlgunElemDe _ []    = False
+perteneceAlgunElemDe [] _    = False
+perteneceAlgunElemDe [x] liY = pertenece x liY
+perteneceAlgunElemDe (x:xs) liY
+    | pertenece x liY = True
+    | otherwise       = perteneceAlgunElemDe xs liY
+
+existeSecuenciaDeAmigos2 :: RedSocial -> Usuario -> Usuario -> Bool
+existeSecuenciaDeAmigos2 rsRed uU1 uU2
+    | (pertenece uU1 (usuarios rsRed) && pertenece uU2 (usuarios rsRed)) == False = False
+    | otherwise = existeSecuenciaDeAmigos2' (usuarios rsRed) uU1 uU2 rsRed
+
+existeSecuenciaDeAmigos2' :: [Usuario] -> Usuario -> Usuario -> RedSocial -> Bool
+existeSecuenciaDeAmigos2' us uU1 uU2 rsRed
+    | not (pertenece uU2 (usuarios rsRed)) || not (pertenece uU1 (usuarios rsRed)) = False
+    | cantidadDeElementosLista (quitar uU2 (amigosDe rsRed uU1)) == 0 || cantidadDeElementosLista (quitar uU1 (amigosDe rsRed uU2)) == 0 = False
+    | otherwise               = cadenaDeAmigos2 (quitar uU2 (amigosDe rsRed uU1)) rsRed
+
+principio :: (Eq t) => [t] -> [t]
+principio [x] = []
+principio (x:xs) = x:principio xs
+
+cadenaDeAmigos2 :: [Usuario] -> RedSocial -> Bool
+cadenaDeAmigos2 [u1, u2] rsRed = relacionadosDirecto u1 u2 rsRed
+cadenaDeAmigos2 (u1:u2:us) rsRed
+    | cantidadDeAmigos rsRed u1 == 0                                                       = False
+    | not (perteneceAlgunElemDe (amigosDe rsRed u2) us) && relacionadosDirecto u1 u2 rsRed = cadenaDeAmigos2 (u1:us) rsRed
+    | relacionadosDirecto u1 u2 rsRed                                                      = cadenaDeAmigos2 (u2:us) rsRed
+    | otherwise                                                                            = cadenaDeAmigos2 ((u1:us) ++ [u2]) rsRed
--- test-catedra.hs
+++ test-catedra.hs
@@ -70,3 +70,204 @@ usuariosB = [usuario1, usuario2, usuario3, usuario5]
 relacionesB = [relacion1_2, relacion2_3]
 publicacionesB = [publicacion1_3, publicacion1_4, publicacion1_5, publicacion3_1, publicacion3_2, publicacion3_3]
 redB = (usuariosB, relacionesB, publicacionesB)
+
+-- Test realizados por nosotros --
+testsEj2 = test [
+    --"Caso 1: red no contiene a u" No se testea porque se trata de un caso de error. No está especificado qué sucede.
+    "Caso 2: red tiene relaciones que no contienen a u. Red contiene a u." ~: amigosDe red2 usuario_2_4 ~?= [],
+    "Caso 3: red tiene relaciones no repetidas y contiene a u." ~: amigosDe red2 usuario_2_1 ~?= [usuario_2_2, usuario_2_3],
+    --"Caso 4: red tiene relaciones repetidas y contiene a u." No se testea porque se trata de un caso de error. No está especificado qué sucede.
+    "Caso 5: red no tiene relaciones. Red contiene a u." ~: amigosDe red2SinRelaciones usuario_2_2 ~?= []
+    ]
+usuario_2_1 = (1, "Juan Ignacio")
+usuario_2_2 = (2, "Ezequiel")
+usuario_2_3 = (3, "Benjamin")
+usuario_2_4 = (4, "Roberto Carlos")
+usuarios2 = [usuario_2_1, usuario_2_2, usuario_2_3, usuario_2_4]
+relacion2_12 = (usuario_2_1, usuario_2_2)
+relacion2_31 = (usuario_2_3, usuario_2_1)
+relaciones2 = [relacion2_12, relacion2_31]
+red2 = (usuarios2, relaciones2, [])
+red2SinRelaciones = (usuarios2, [], [])
+
+testsEj3 = test [
+    --"Caso 1: red no contiene a u" No se testea porque se trata de un caso de error. No está especificado qué sucede.
+    "Caso 2: red tiene relaciones que no contienen a u. Red contiene a u." ~: cantidadDeAmigos red2 usuario_2_4 ~?= 0,
+    "Caso 3: red tiene relaciones no repetidas y contiene a u." ~: cantidadDeAmigos red2 usuario_2_1 ~?= 2,
+    --"Caso 4: red tiene relaciones repetidas y contiene a u." No se testea porque se trata de un caso de error. No está especificado qué sucede.
+    "Caso 5: red no tiene relaciones. Red contiene a u." ~: cantidadDeAmigos red2SinRelaciones usuario_2_2 ~?= 0
+    ]
+
+testsEj4 = test [
+    --"Caso 1: la cantidad de usuarios de la red es 0"  No se testea porque se trata de un caso de error. No está especificado qué sucede.
+    "Caso 2: la cantidad de usuarios de la red es mayor a 1 y red no tiene relaciones" ~: usuarioConMasAmigos red2SinRelaciones ~?= usuario_2_1,
+    "Caso 3: la cantidad de usuarios de la red es mayor a 1 y red tiene relaciones, ninguna repetida" ~: usuarioConMasAmigos red2 ~?= usuario_2_1
+    --"Caso 4: la cantidad de usuarios de la red es mayor a 1 y red tiene relaciones, con repetidas" No se testea porque se trata de un caso de error. No está especificado qué sucede.
+    ]
+
+testsEj5 = test [
+    "Caso 1: La cantidad máxima de amigos de un usuario es menor a 10." ~: estaRobertoCarlos red2SinRelaciones ~?= False,
+    "Caso 2: La cantidad máxima de amigos de un usuario es 10." ~: estaRobertoCarlos red5SinRobertoCarlos ~?= False,
+    "Caso 3: La cantidad máxima de amigos de un usuario es mayor a 10." ~: estaRobertoCarlos red5ConRobertoCarlos ~?= True
+    ]
+usuario_5_5 = (5, "Javier")
+usuario_5_6 = (6, "Gabriel")
+usuario_5_7 = (7, "Franco")
+usuario_5_8 = (8, "Mariela")
+usuario_5_9 = (9, "Natalia")
+usuario_5_10 = (10, "Liliana")
+usuario_5_11 = (11, "Miguel")
+usuario_5_12 = (12, "Eda")
+usuarios5 = [usuario_2_1, usuario_2_2, usuario_2_3, usuario_2_4, usuario_5_5, usuario_5_6, usuario_5_7, usuario_5_8, usuario_5_9, usuario_5_10, usuario_5_11, usuario_5_12]
+relacion_5_41 = (usuario_2_4, usuario_2_1)
+relacion_5_42 = (usuario_2_4, usuario_2_2)
+relacion_5_43 = (usuario_2_4, usuario_2_3)
+relacion_5_45 = (usuario_2_4, usuario_5_5)
+relacion_5_46 = (usuario_2_4, usuario_5_6)
+relacion_5_47 = (usuario_2_4, usuario_5_7)
+relacion_5_48 = (usuario_2_4, usuario_5_8)
+relacion_5_49 = (usuario_2_4, usuario_5_9)
+relacion_5_410 = (usuario_2_4, usuario_5_10)
+relacion_5_411 = (usuario_2_4, usuario_5_11)
+relacion_5_412 = (usuario_2_4, usuario_5_12)
+relaciones5Con10Amigos = [relacion_5_41, relacion_5_42, relacion_5_43, relacion_5_45, relacion_5_46, relacion_5_47, relacion_5_48, relacion_5_49, relacion_5_410, relacion_5_411]
+relaciones5Con11Amigos = [relacion_5_41, relacion_5_42, relacion_5_43, relacion_5_45, relacion_5_46, relacion_5_47, relacion_5_48, relacion_5_49, relacion_5_410, relacion_5_411
+                         , relacion_5_412]
+red5SinRobertoCarlos = (usuarios5, relaciones5Con10Amigos, [])
+red5ConRobertoCarlos = (usuarios5, relaciones5Con11Amigos, [])
+
+testsEj6 = test [
+    --"Caso 1: red no contiene a u" No se testea porque se trata de un caso de error. No está especificado qué sucede.
+    "Caso 2: red no tiene publicaciones" ~: publicacionesDe red5ConRobertoCarlos usuario_2_4 ~?= [],
+    "Caso 3: red tiene publicaciones y u no." ~: publicacionesDe red6 usuario_2_3 ~?= [],
+    --"Caso 4: red tiene publicaciones y u tiene repetidas" No se testea porque se trata de un caso de error. No está especificado qué sucede.
+    "Caso 5: red tiene publicaciones y u tiene, sin repetidas" ~: publicacionesDe red6 usuario_2_1 ~?= [publicacion6_1, publicacion6_2, publicacion6_3, publicacion6_4, publicacion6_5]
+    ]
+publicacion6_1 = (usuario_2_1, "Este es mi primer post", [usuario_2_2, usuario_2_3, usuario_2_4])
+publicacion6_2 = (usuario_2_1, "Este es mi segundo post", [usuario_2_2, usuario_2_3, usuario_2_4])
+publicacion6_3 = (usuario_2_1, "Este es mi tercer post", [usuario_2_2])
+publicacion6_4 = (usuario_2_1, "Este es mi cuarto post", [usuario_2_2])
+publicacion6_5 = (usuario_2_1, "Este es como mi quinto post", [usuario_2_2])
+publicacion6_6 = (usuario_2_4, "Hola red", [usuario_2_1])
+publicacion6_7 = (usuario_2_2, "Hello world", [])
+publicaciones6 = [publicacion6_1, publicacion6_2, publicacion6_3, publicacion6_4, publicacion6_5, publicacion6_6, publicacion6_7]
+red6 = (usuarios2, relaciones2, publicaciones6)
+
+testsEj7 = test [
+    --"Caso 1: red no contiene a u" No se testea porque se trata de un caso de error. No está especificado qué sucede.
+    "Caso 2: red no tiene publicaciones" ~: publicacionesQueLeGustanA red5ConRobertoCarlos usuario_2_4 ~?= [],
+    "Caso 3: red tiene publicaciones y a u no le gusta ninguna" ~: publicacionesQueLeGustanA redB usuario1 ~?= [],
+    "Caso 4: Caso 4: red tiene publicaciones y a u le gusta alguna" ~: publicacionesQueLeGustanA redB usuario2 ~?= [publicacion1_3, publicacion3_2, publicacion3_3]
+    ]
+
+testsEj8 = test[
+    --"Caso 1: red no contiene a u1 o u2" No se testea porque se trata de un caso de error. No está especificado qué sucede.
+    "Caso 2: red no tiene publicaciones" ~: lesGustanLasMismasPublicaciones red5ConRobertoCarlos usuario_2_2 usuario_2_4 ~?= True,
+    "Caso 3: red tiene publicaciones y ni a u1 ni a u2 le gusta ninguna" ~: lesGustanLasMismasPublicaciones redB usuario1 usuario3 ~?= True,
+    "Caso 4: red tiene publicaciones y uno solo de los dos usuarios tiene publicaciones que le gustan, mientras el otro no tiene ninguna" ~: lesGustanLasMismasPublicaciones redB usuario2 usuario1 ~?= False,
+    "Caso 5: red tiene publicaciones y ambos usuarios tienen publicaciones que les gustan en común pero, también tienen algunas que les gustan a ellos solos" ~: lesGustanLasMismasPublicaciones redB usuario2 usuario5 ~?= False,
+    "Caso 6: red tiene publicaciones y a u1 y u2 les gustan todas las mismas" ~: lesGustanLasMismasPublicaciones red6 usuario_2_3 usuario_2_4 ~?= True
+    ]
+
+
+
+usuario900 = (1, "u1")
+usuario901 = (2, "u2")
+usuario902 = (3, "u3")
+usuario903 = (4, "u4") -- Sin publicaiones
+usuario904 = (5, "u5")
+
+publicacion900_1 = (usuario900, "pA", []) -- Publicación sin likes
+publicacion900_2 = (usuario900, "pB", [usuario900,usuario901])
+publicacion900_3 = (usuario900, "pC", [usuario902])
+publicacion900_4 = (usuario900, "pD", [usuario902])
+publicacion900_5 = (usuario900, "pE", [usuario902])
+
+usuariosTEST_EJ9A = [usuario900]
+usuariosTEST_EJ9C = [usuario900, usuario901, usuario903]
+usuariosTEST_EJ9D = [usuario900, usuario901, usuario902]
+usuariosTEST_EJ9E = [usuario900, usuario902]
+
+publicacionesTEST_EJ9B = [publicacion900_1]
+publicacionesTEST_EJ9C = [publicacion900_1, publicacion900_2]
+publicacionesTEST_EJ9D = [publicacion900_2, publicacion900_3, publicacion900_4]
+publicacionesTEST_EJ9E = [publicacion900_3, publicacion900_4, publicacion900_5]
+
+redTEST_EJ9A = (usuariosTEST_EJ9A,[],[])
+redTEST_EJ9B = (usuariosTEST_EJ9A, [], publicacionesTEST_EJ9B)
+redTEST_EJ9C = (usuariosTEST_EJ9C, [], publicacionesTEST_EJ9C)
+redTEST_EJ9D = (usuariosTEST_EJ9D, [], publicacionesTEST_EJ9D)
+redTEST_EJ9E = (usuariosTEST_EJ9E, [], publicacionesTEST_EJ9E)
+testEj9 = test[
+    "Caso 1: la red no tiene publicaciones"                                                    ~: tieneUnSeguidorFiel redTEST_EJ9A usuario900 ~?= False,
+    "Caso 2: la red tiene menos de 2 usuarios diferentes"                                      ~: tieneUnSeguidorFiel redTEST_EJ9B usuario900 ~?= False,
+    "Caso 3: el usuario no tiene publicaciones en esta red"                                    ~: tieneUnSeguidorFiel redTEST_EJ9C usuario903 ~?= False,
+    "Caso 4: no existe un usuario u2 al que le gusten TODAS las publicaciones de u1"           ~: tieneUnSeguidorFiel redTEST_EJ9D usuario900 ~?= False,
+    "Caso 5: existe por lo menos un usuario u2 al que le gustan TODAS las publicaciones de u1" ~: tieneUnSeguidorFiel redTEST_EJ9E usuario900 ~?= True
+ ]
+
+usuario1000 = (1000, "A")
+usuario1001 = (1001, "B")
+usuario1002 = (1002, "C")
+usuario1003 = (1003, "D")
+usuario1004 = (1004, "E")
+usuario1005 = (1005, "F") -- Este usuario debe estar relacionados con todos (excepto 1006)
+usuario1006 = (1006, "G") -- Este usuario no debe estar relacionado con ningun otro
+
+relacion1000_1001 = (usuario1000, usuario1001)
+relacion1001_1002 = (usuario1001, usuario1002)
+relacion1002_1003 = (usuario1002, usuario1003)
+relacion1003_1004 = (usuario1003, usuario1004)
+
+relacion1005_1000 = (usuario1005, usuario1000)
+relacion1005_1001 = (usuario1005, usuario1001)
+relacion1005_1002 = (usuario1005, usuario1002)
+
+relacionesTestEJ10A = [relacion1000_1001]
+usuariosTestEJ10A = [usuario1000, usuario1001]
+redTestEJ10A = (usuariosTestEJ10A, relacionesTestEJ10A, [])
+
+relacionesTestEJ10B = [relacion1000_1001, relacion1001_1002]
+usuariosTestEJ10B = [usuario1000, usuario1001, usuario1002]
+redTestEJ10B = (usuariosTestEJ10B, relacionesTestEJ10B, [])
+
+relacionesTestEJ10C = [relacion1000_1001, relacion1001_1002, relacion1002_1003, relacion1003_1004]
+usuariosTestEJ10C_Per1 = [usuario1000, usuario1001, usuario1002, usuario1003, usuario1004]
+usuariosTestEJ10C_Per2 = [usuario1000, usuario1001, usuario1003, usuario1002, usuario1004]
+usuariosTestEJ10C_Per3 = [usuario1000, usuario1002, usuario1001, usuario1003, usuario1004]
+usuariosTestEJ10C_Per4 = [usuario1000, usuario1002, usuario1003, usuario1001, usuario1004]
+usuariosTestEJ10C_Per5 = [usuario1000, usuario1003, usuario1001, usuario1002, usuario1004]
+usuariosTestEJ10C_Per6 = [usuario1000, usuario1003, usuario1002, usuario1001, usuario1004]
+redTestEJ10C = (usuariosTestEJ10C_Per1, relacionesTestEJ10C, [])
+
+relacionesTestEJ10D = [relacion1000_1001, relacion1001_1002, relacion1002_1003, relacion1003_1004]
+usuariosTestEJ10D = [usuario1000, usuario1001, usuario1002, usuario1006, usuario1003, usuario1004]
+redTestEJ10D = (usuariosTestEJ10D, relacionesTestEJ10D, [])
+
+relacionesTestEJ10E = [relacion1000_1001, relacion1001_1002, relacion1005_1000, relacion1005_1001, relacion1005_1002]
+usuariosTestEJ10E_Per1A = [usuario1000, usuario1001, usuario1002, usuario1005]
+usuariosTestEJ10E_Per1B = [usuario1000, usuario1002, usuario1001, usuario1005]
+usuariosTestEJ10E_Per2A = [usuario1005, usuario1001, usuario1002, usuario1000]
+usuariosTestEJ10E_Per2B = [usuario1005, usuario1002, usuario1001, usuario1000]
+redTestEJ10E = (usuariosTestEJ10E_Per1A, relacionesTestEJ10E, [])
+
+-- Para testear este ejercicio los elementos de los extremos del
+-- parametro [Usuarios] no se deben permutar (ya que eso nunca puede ocurrir)
+-- por ejemplo si testeamos [u1, u2, u3, u4], tambien debemos testear [u1, u3, u2, u4]
+-- si fuera [u1, u2, u3, u4, u5] testeamos [u1, (permutaciones de [u2,u3,u4]), u5]
+testCadenaDeAmigos = runTestTT testEJ10A
+testEJ10A = test [
+    " cadenaDeAmigos 2U, 1R V" ~: (cadenaDeAmigos2 usuariosTestEJ10A redTestEJ10A) ~?= True,
+    " cadenaDeAmigos 3U, 2R V" ~: (cadenaDeAmigos2 usuariosTestEJ10B redTestEJ10B) ~?= True,
+    " cadenaDeAmigos 5U, 4R P1 V" ~: (cadenaDeAmigos2 usuariosTestEJ10C_Per1 redTestEJ10C) ~?= True,
+    " cadenaDeAmigos 5U, 4R P2 V" ~: (cadenaDeAmigos2 usuariosTestEJ10C_Per2 redTestEJ10C) ~?= True,
+    " cadenaDeAmigos 5U, 4R P3 V" ~: (cadenaDeAmigos2 usuariosTestEJ10C_Per3 redTestEJ10C) ~?= True,
+    " cadenaDeAmigos 5U, 4R P4 V" ~: (cadenaDeAmigos2 usuariosTestEJ10C_Per4 redTestEJ10C) ~?= True,
+    " cadenaDeAmigos 5U, 4R P5 V" ~: (cadenaDeAmigos2 usuariosTestEJ10C_Per5 redTestEJ10C) ~?= True,
+    " cadenaDeAmigos 5U, 4R P6 V" ~: (cadenaDeAmigos2 usuariosTestEJ10C_Per6 redTestEJ10C) ~?= True,
+    " un usuario no tiene ninguna relacion F" ~: (cadenaDeAmigos2 usuariosTestEJ10D redTestEJ10D) ~?= False,
+    " un usuario tiene a todos en su lista de amigos A V" ~: (cadenaDeAmigos2 usuariosTestEJ10E_Per1A redTestEJ10E) ~?= True,
+    " un usuario tiene a todos en su lista de amigos B V" ~: (cadenaDeAmigos2 usuariosTestEJ10E_Per1B redTestEJ10E) ~?= True,
+    " un usuario tiene a todos en su lista de amigos C V" ~: (cadenaDeAmigos2 usuariosTestEJ10E_Per2A redTestEJ10E) ~?= True,
+    " un usuario tiene a todos en su lista de amigos D V" ~: (cadenaDeAmigos2 usuariosTestEJ10E_Per2B redTestEJ10E) ~?= True
+ ]
